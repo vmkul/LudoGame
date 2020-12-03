28b9ffbefc8725d0dd96a91481097d52eb8a2883
vmkul <55065764+vmkul@users.noreply.github.com>
2020-12-03 23:20:22 +0200

    Add board coordinates

---
+++ squareCoordinates.js
@@ -0,0 +1,298 @@
+'use strict';
+
+const squareCoordinates = [
+  {
+    left: 426,
+    bottom: 100,
+  },
+  {
+    left: 426,
+    bottom: 166,
+  },
+  {
+    left: 426,
+    bottom: 229,
+  },
+  {
+    left: 426,
+    bottom: 294,
+  },
+  {
+    left: 426,
+    bottom: 358,
+  },
+  {
+    left: 361,
+    bottom: 422,
+  },
+  {
+    left: 298,
+    bottom: 422,
+  },
+  {
+    left: 234,
+    bottom: 422,
+  },
+  {
+    left: 171,
+    bottom: 422,
+  },
+  {
+    left: 107,
+    bottom: 422,
+  },
+  {
+    left: 42,
+    bottom: 422,
+  },
+  {
+    left: 42,
+    bottom: 486,
+  },
+  {
+    left: 42,
+    bottom: 551,
+  },
+  {
+    left: 105,
+    bottom: 551,
+  },
+  {
+    left: 170,
+    bottom: 551,
+  },
+  {
+    left: 233,
+    bottom: 551,
+  },
+  {
+    left: 298,
+    bottom: 551,
+  },
+  {
+    left: 362,
+    bottom: 551,
+  },
+  {
+    left: 429,
+    bottom: 614,
+  },
+  {
+    left: 429,
+    bottom: 679,
+  },
+  {
+    left: 429,
+    bottom: 742,
+  },
+  {
+    left: 429,
+    bottom: 807,
+  },
+  {
+    left: 429,
+    bottom: 871,
+  },
+  {
+    left: 429,
+    bottom: 935,
+  },
+  {
+    left: 489,
+    bottom: 935,
+  },
+  {
+    left: 554,
+    bottom: 935,
+  },
+  {
+    left: 554,
+    bottom: 872,
+  },
+  {
+    left: 554,
+    bottom: 808,
+  },
+  {
+    left: 554,
+    bottom: 743,
+  },
+  {
+    left: 554,
+    bottom: 679,
+  },
+  {
+    left: 554,
+    bottom: 616,
+  },
+  {
+    left: 619,
+    bottom: 551,
+  },
+  {
+    left: 683,
+    bottom: 551,
+  },
+  {
+    left: 748,
+    bottom: 551,
+  },
+  {
+    left: 813,
+    bottom: 551,
+  },
+  {
+    left: 875,
+    bottom: 551,
+  },
+  {
+    left: 940,
+    bottom: 551,
+  },
+  {
+    left: 940,
+    bottom: 486,
+  },
+  {
+    left: 940,
+    bottom: 423,
+  },
+  {
+    left: 876,
+    bottom: 423,
+  },
+  {
+    left: 812,
+    bottom: 423,
+  },
+  {
+    left: 749,
+    bottom: 423,
+  },
+  {
+    left: 684,
+    bottom: 423,
+  },
+  {
+    left: 619,
+    bottom: 423,
+  },
+  {
+    left: 555,
+    bottom: 359,
+  },
+  {
+    left: 555,
+    bottom: 294,
+  },
+  {
+    left: 555,
+    bottom: 230,
+  },
+  {
+    left: 555,
+    bottom: 166,
+  },
+  {
+    left: 555,
+    bottom: 102,
+  },
+  {
+    left: 555,
+    bottom: 37,
+  },
+  {
+    left: 492,
+    bottom: 37,
+  },
+  {
+    left: 427,
+    bottom: 37,
+  },
+];
+
+const redSquares = [
+  {
+    left: 491,
+    bottom: 100,
+  },
+  {
+    left: 491,
+    bottom: 166,
+  },
+  {
+    left: 491,
+    bottom: 230,
+  },
+  {
+    left: 491,
+    bottom: 294,
+  },
+  {
+    left: 491,
+    bottom: 357,
+  },
+];
+
+const yellowSquares = [
+  {
+    left: 491,
+    bottom: 871,
+  },
+  {
+    left: 491,
+    bottom: 808,
+  },
+  {
+    left: 491,
+    bottom: 744,
+  },
+  {
+    left: 491,
+    bottom: 679,
+  },
+  {
+    left: 491,
+    bottom: 614,
+  },
+];
+
+const redHome = [
+  {
+    left: 202,
+    bottom: 133,
+  },
+  {
+    left: 136,
+    bottom: 197,
+  },
+  {
+    left: 265,
+    bottom: 197,
+  },
+  {
+    left: 202,
+    bottom: 261,
+  },
+];
+
+const yellowHome = [
+  {
+    left: 779,
+    bottom: 710,
+  },
+  {
+    left: 715,
+    bottom: 774,
+  },
+  {
+    left: 845,
+    bottom: 774,
+  },
+  {
+    left: 781,
+    bottom: 839,
+  },
+]
+
+export { squareCoordinates, redSquares, yellowSquares, redHome, yellowHome };
